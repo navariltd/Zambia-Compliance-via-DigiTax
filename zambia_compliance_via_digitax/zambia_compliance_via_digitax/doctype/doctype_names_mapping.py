1cdfc3c329d07951bc4b707c1d9fd0d57c3ede32
Brian <mwambia@navari.co.ke>
2026-02-24 12:42:21 +0300

    refactor(doctype): remove  prefix from most doctypes
    - Rename doctypes like  →  for simplicity
    - Reduce typing and improve consistency across the system

--- zambia_compliance_via_digitax/zambia_compliance_via_digitax/doctype/doctype_names_mapping.py
+++ zambia_compliance_via_digitax/zambia_compliance_via_digitax/doctype/doctype_names_mapping.py
@@ -1,3 +1,3 @@
-SETTINGS_DOCTYPE_NAME = "Navari ZRA SIS Settings"  # Holds credentials, URLs, tokens
-ROUTES_TABLE_CHILD_DOCTYPE_NAME ="Navari ZRA SIS Route Item"
-ROUTES_TABLE_DOCTYPE_NAME ="Navari ZRA SIS Routes"
+SETTINGS_DOCTYPE_NAME = "ZRA SIS Settings"  # Holds credentials, URLs, tokens
+ROUTES_TABLE_CHILD_DOCTYPE_NAME ="ZRA SIS Route Item"
+ROUTES_TABLE_DOCTYPE_NAME ="ZRA SIS Routes"
